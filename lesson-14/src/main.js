5e010223faccf9bf13ad8a23dfe1f7714012257a
= <=>
2026-06-23 18:50:24 -0600

    completed lesson 14

--- lesson-14/src/main.js
+++ lesson-14/src/main.js
@@ -1,1 +1,8 @@
-console.log('Lesson 14 starter loaded');
+import dayjs from 'dayjs';
+import { greeting } from './utils'; // importing function from utils.js
+import utils from './utils'; // imports the default name
+// utils can be any name you like
+const currentDate = dayjs().format('dddd, MMMM D, YYYY');
+const name = prompt('Enter your name:');
+document.querySelector('#greeting').textContent = greeting(name || utils.defaultName);
+document.querySelector('#today').textContent = `Today is ${currentDate}`;
---
+++ lesson-14/src/utils.js
@@ -0,0 +1,4 @@
+export function greeting(name) { // export a function
+  return `Greetings ${name}`;
+}
+export default { defaultName: 'John Cena' }; // exporting js object
